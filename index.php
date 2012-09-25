4b1c033db45f40cd877867aa13263bfad6c906f7
riname <riname@laposte.net>
2012-09-25 14:30:39 +0200

    add kgraph

--- index.php
+++ index.php
@@ -1,12 +1,172 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+<title></title>		  
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="content-language" content="fr" />
+<link href="style.css"	title="Défaut" rel="stylesheet" type="text/css" media="screen" />
+	<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
+	<script type="text/javascript" src="js/jquery-easing-1.3.pack.js"></script>
+	<script type="text/javascript" src="js/jquery-easing-compatibility.1.2.pack.js"></script>
+	<script type="text/javascript" src="js/coda-slider.1.1.1.pack.js"></script>
+	<script type="text/javascript" src="js/js.js"></script>
+<!--[if lt IE 10]> 
+<style>
+.cadre    {behavior: url(js/PIE.php);}
+</style>
+<![endif]-->
+</head>
+<body> 
+
+       <div id="conteneur">
+<!-- *************************************** HEADER **************************************** -->	 
+            <div id="header">	
+                 <div id="header_haut">
+                  <a href=""><img src="images/souscrire.png" width="100" height="17" alt="" /></a>
+				 </div>	
+			
+ 				 <ul id="menu_header">
+                     <li id="ici"><a href="#" title="">Rubrique</a></li>
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li>  	
+                     <li><a href="#" title="">Rubrique</a></li> 
+                     <li><a href="#" title="">Rubrique</a></li> 
+                 </ul>	 
+				 
+				 <a id="twitter" href="#"></a>
+			</div>
+<!-- *************************************** HEADER **************************************** -->   
+
+<!-- *************************************** CONTENU **************************************** -->	 
+	   <div id="contenu">  
+	   <div id="cadre">	   
+            <div class="cadre">
+                 Lorem ipsum dolor sit amet, consetetu 
+                 adipiscing elit. Curabitur interdum orci 
+                 non neque placerat sodales. 
+				 <br /><br />
+                 Cum sociis natoque penatibus et mag 
+                 dis parturie montes, nascetur ridiculus. 
+				 <br /><br />
+                 Morbi tempor ultrices erat tristique 
+                 blandit. Donec volutpat urna nec arcu 
+                 ultricies in semper eros pulvinar. 
+                 Etiam sed nisl nulla. Duis eu libero nec 
+                 metus imperdiet interdum sit amet ut. 
+                 Donec suscipit congue varius. 
+				 <br /><br />
+                 Aenean suscipit ante a nibh dignissim.
+			</div>
+			
+            <div class="cadre">
+                 Lorem ipsum dolor sit amet, consetetu 
+                 adipiscing elit. Curabitur interdum orci 
+                 non neque placerat sodales. 
+				 <br /><br />
+                 Cum sociis natoque penatibus et mag 
+                 dis parturie montes, <a href="" title="">nascetur ridiculus.</a>
+				 <br /><br />
+                 Morbi tempor ultrices erat tristique 
+                 blandit. Donec volutpat urna nec arcu 
+                 ultricies in semper eros pulvinar. 
+                 Etiam sed nisl nulla. Duis eu libero nec 
+                 metus imperdiet interdum sit amet ut. 
+                 Donec suscipit congue varius. 
+				 <br /><br />
+                 Aenean suscipit ante a nibh dignissim.
+			</div> 
+
+            <div class="cadre1">
+                  <a href=""><img style="margin:8px 0" src="images/pub1.png" alt="" /></a>
+				  <br />  
+                  <a href=""><img src="images/pub2.png" alt="" /></a>	
+			</div> 
+           <div class="clear"></div>			 
+	   </div>	
+<br />   
+<!-- ****************** SLIDE ********************* -->	
+<div id="contenu_couleur">
+<div id="page-wrap">
+	<div class="slider-wrap">
+		<div id="main-photo-slider" class="csw">
+			<div class="panelContainer">
+<!-- ************* IMG 1 **************** -->
+				<div class="panel" title="Panel 1">
+					 <div class="wrapper">
+						  <img src="img_slide/photo-1.jpg" alt="temp" />
+						  <div class="photo-meta-data">
+							   Photo Credit: <a href="http://www.sxc.hu/photo/1240770">marchecco</a><br />
+							   <span>Sfondo Astratto colorato 1 Sfondo astratto con diversi colori</span>
+						  </div>
+					 </div> 
+				</div>
+<!-- ************* IMG 1 **************** -->
+<!-- ************* IMG 2 **************** -->
+				<div class="panel" title="Panel 2">
+					 <div class="wrapper">
+						  <img src="img_slide/photo-2.jpg" alt="temp" />
+						  <div class="photo-meta-data">
+							   Photo Credit: <a href="http://www.sxc.hu/photo/1063683">ilco</a><br />
+							   <span>night lights modern building</span>
+						  </div>
+					 </div>
+				</div>	
+<!-- ************* IMG 2 **************** -->
+<!-- ************* IMG 3 **************** -->
+				<div class="panel" title="Panel 3">
+					<div class="wrapper">
+						  <img src="img_slide/photo-3.jpg" alt="temp" />
+						  <div class="photo-meta-data">
+							   Photo Credit: <a href="http://www.sxc.hu/photo/1261909">shane_shoe</a><br />	
+							   <span>Frosted Mind 1 abstract graphic</span>
+						  </div>   
+					</div>
+				</div>	
+<!-- ************* IMG 3 **************** -->
+<!-- ************* IMG 4 **************** -->
+				<div class="panel" title="Panel 4">
+					<div class="wrapper">
+						 <img src="img_slide/photo-4.jpg" alt="temp" />
+						 <div class="photo-meta-data">
+							   Photo Credit: <a href="http://www.sxc.hu/photo/1168081">Maddenfrs</a><br />	
+							   <span>Ring of Light A glowing ring of light.</span>
+						 </div>
+					</div>
+				</div>
+<!-- ************* IMG 4 **************** -->
+			</div>
+		</div>
+	</div>		 
+		<div id="movers-row">  
+            <div>		
+		    <a href="#1" class="cross-link"><img src="img_slide/photo-1thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a><!-- ************* Petite IMG 1 **************** -->
+			<a href="#2" class="cross-link"><img src="img_slide/photo-2thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a><!-- ************* Petite IMG 2 **************** --> 
+			</div>	  
+            <div>
+			<a href="#3" class="cross-link"><img src="img_slide/photo-3thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a><!-- ************* Petite IMG 3 **************** -->
+			<a href="#4" class="cross-link"><img src="img_slide/photo-4thumb.jpg" class="nav-thumb" alt="temp-thumb" /></a><!-- ************* Petite IMG 4 **************** -->
+			</div>
+		</div>
+	</div> 
+</div>
+<!-- ****************** SLIDE ********************* -->	
+	   </div>  
+<!-- *************************************** CONTENU **************************************** -->  
+
+<!-- ***************************************** PIED ****************************************** -->	 
+            <div id="pied"></div>  
+				 <!-- mention de copyright Ne pas retirer sans autorisation écrite Design <a href="http://www.kitgraphiquegratuit.org" onclick="window.open(this.href); return false;" title="kits graphiques gratuits" > Kit graphique</a>-->	
+			     <div class="copyright">	©<a href=""> Votresite.com</a> | Design <a href="http://www.kitgraphiquegratuit.org" onclick="window.open(this.href); return false;" title="kits graphiques gratuits" > Kit graphique</a> | <a href="http://validator.w3.org/check?uri=referer" onclick="window.open(this.href); return false;">XHTML strict</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" onclick="window.open(this.href); return false;">CSS3</a></div>
+                 <!-- mention de copyright Ne pas retirer sans autorisation écrite -->
+
+<!-- ***************************************** PIED ****************************************** -->
+	   </div>
+
+</body>
 </html>
+
+
